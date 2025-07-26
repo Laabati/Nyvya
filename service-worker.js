dc38d1ea4ca66f0f7c4535710777c2a5e2c53bcf
Laabati <nyvya.contact@gmail.com>
2025-07-26 13:09:46 +0200

    Add service-worker.js: precache and fetch handlers

--- service-worker.js
+++ service-worker.js
@@ -18,13 +18,14 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
+    caches.match(event.request)
+      .then(cached => {
+        return cached || fetch(event.request)
+          .then(networkResponse => {
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, networkResponse.clone()));
+            return networkResponse;
+          });
+      })
   );
 });
